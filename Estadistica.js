79a2b6e73af4b20abd01d74edd1d66367a41abb4
Daniela Saavedra <danielasaavedraprieto@gmail.com>
2022-03-29 11:58:18 -0500

    geometria terminado

---
+++ Estadistica.js
@@ -0,0 +1,12 @@
+let lista1 = [
+    100,
+    200,
+    300,
+    500,
+];
+
+let sumalista = 0;
+
+for (let i = 0; i < lista1.length; i++){
+    sumalista = sumalista + lista1[i];
+}
--- FigurasGeometria.js
+++ FigurasGeometria.js
@@ -4,21 +4,27 @@ function calPeriCuadrado(){
     let valorInput = Number(input.value);
 
     let perimetro = valorInput *4;
-    alert(perimetro)
+
+    const resultCuadr = document.getElementById("resultCuadr")
+    resultCuadr.innerText = "El perímetro del cuadrado es " + perimetro + " cm";
 }
 function calAreaCuadrado(){
     let input = document.getElementById("ladoCuadrado");
     let valorInput = Number(input.value);
 
     let area = valorInput *valorInput;
-    alert(area);
+    
+    const resultCuadr = document.getElementById("resultCuadr")
+    resultCuadr.innerText = "El área del cuadrado es " + area + " cm2";
 }
 function calVolCuadrado(){
     let input = document.getElementById("ladoCuadrado");
     let valorInput = Number(input.value);
 
     let vol = valorInput *valorInput * valorInput;
-    alert(vol)
+    
+    const resultCuadr = document.getElementById("resultCuadr")
+    resultCuadr.innerText = "El volumen del cuadrado es " + vol + " cm3";
 }
 //funciones del triangulo
 function calPeriTria(){
@@ -33,7 +39,8 @@ function calPeriTria(){
 
     let perimetro = valorLado1 + valorLado2 + valorBase;
 
-    alert(perimetro)
+    const result = document.getElementById("resultTria")
+    result.innerText = "El perímetro del triangulo es " + perimetro + " cm";
 }
 
 function calAreaTria(){
@@ -44,9 +51,10 @@ function calAreaTria(){
     let valorBase = Number(base.value);
 
     let altura = Math.sqrt((valorLado1*valorLado1)-(valorBase/2)*(valorBase/2));
-    let area = (valorBase * altura)/2
+    let area = ((valorBase * altura)/2).toFixed(2);
 
-    alert(area);
+    const result = document.getElementById("resultTria")
+    result.innerText = "El área del triangulo es " + area + " cm2";
 }
 function calVolTria(){
     let lado1 = document.getElementById("ladoTria1");
@@ -59,9 +67,10 @@ function calVolTria(){
     let valorBase = Number(base.value);
 
     let altura = Math.sqrt((valorLado1*valorLado1)-(valorBase/2)*(valorBase/2));
-    let vol = valorancho * valorBase * altura;
+    let vol = (valorancho * valorBase * altura).toFixed(2);
 
-    alert(vol);
+    const result = document.getElementById("resultTria")
+    result.innerText = "El volumen del triangulo es " + vol + " cm3";
 }
 //funciones del circulo
 function calPeriCir(){
@@ -69,19 +78,25 @@ function calPeriCir(){
     let valorRadio = Number(radio.value);
 
     let perimetro = (2 * Math.PI * valorRadio).toFixed(2);
-    alert(perimetro)
+    
+    const result = document.getElementById("resultCir")
+    result.innerText = "El perímetro del circulo es " + perimetro + " cm";
 }
 function calAreaCir(){
     let radio = document.getElementById("radioC");
     let valorRadio = Number(radio.value);
 
     let area = (Math.PI * valorRadio * valorRadio).toFixed(2);
-    alert(area);
+    
+    const result = document.getElementById("resultCir")
+    result.innerText = "El área del circulo es " + area + " cm2";
 }
 function calVolCir(){
     let radio = document.getElementById("radioC");
     let valorRadio = Number(radio.value);
 
     let vol = ((4 * Math.PI * valorRadio * valorRadio * valorRadio)/3).toFixed(2);
-    alert(vol)
+    
+    const result = document.getElementById("resultCir")
+    result.innerText = "El volumen del circulo es " + vol + " cm3";
 }
